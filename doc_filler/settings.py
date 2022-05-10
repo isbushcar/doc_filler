f95b31124b1871465e2c55edcf5b612be757e7f9
Andrey Berezin <isbushcar@gmail.com>
2022-05-10 17:22:56 +0300

    Remove secret key from settings.py

--- doc_filler/settings.py
+++ doc_filler/settings.py
@@ -28,10 +28,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5_vuge0cctxilcniqqebu8y&vxu337dp)^%mw$z5a7n=(ldzn4'  # TODO: remove
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['localhost']
 
